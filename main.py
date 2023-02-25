299cd0324c872dd6ab5020ce4d9b5ddc3388f0c8
Rushi Patel <125636158+RushiPatel78@users.noreply.github.com>
2023-02-25 21:43:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ password=None
 
 app.config["MAIL_SERVER"]='smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USERNAME"] = 'dailytv.com25@gmail.com'
-app.config['MAIL_PASSWORD'] = 'test@7820'
+app.config["MAIL_USERNAME"] = 'email'
+app.config['MAIL_PASSWORD'] = 'password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 # mail = mail(app)
